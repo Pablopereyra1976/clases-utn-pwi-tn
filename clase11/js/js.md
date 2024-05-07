2350aae82a5e1a8dc0b7fce9fc71e6372d9a0b65
Pablo Pereyra <pablopereyra1976@hotmail.com>
2024-05-06 21:57:16 -0300

    clase11-introduccion a js

---
+++ clase11/js/js.md
@@ -0,0 +1,6 @@
+que es javascript?
+
+es un lenguaje de programacion interpretado por el v8engine.
+es de alto nivel.(mas parecido al lenguaje humano)
+se ejecuta en el navegador.
+es un lenguaje CASE SENSITIVE diferencia entre mayus y minus.
---
+++ clase11/js/script.js
@@ -0,0 +1,53 @@
+console.log("hola mundo");
+/*tipo de datos*/
+
+/* datos primitivos 
+number: numeros (8 , 2 , 22 ,3.14(float), NaN, infinity)
+
+string: textos ("hola mundo" , 'pepe' , `8`) siempre entre comillas.
+
+boolean: true , false (representan un estado booleano) ¿te gusta? si o no, true o false.
+
+null: ausencia o nulidad de un dato (0, "", [], {})
+
+undefined: no hay dato definido (undefined).
+
+*/
+/* constructores */
+/* string(dato) transforma el dato en string
+number(dato) transforma el dato en number
+*/
+
+
+/* operadores aritmeticos */
+/* + concatenacion */
+/* devuelven numeros
++ SUMA
+- RESTA
+* MULTIPLICACION
+/ DIVISION
+
+typeof: devuelve el tipo de dato
+NaN: tipo de dato numerico, no es un numero (nose que es esto)
+isNaN: devuelve un booleno dependiendo de si es un NaN o no
+ */
+
+
+/* comparadores (siempre devuelven booleanos)
+==: igual
+===:estrictamente igual
+!==:distinto
+!===:estrictamente distinto
+> mayor que
+< menor que
+>= mayor o igual que
+<= menor o igual que
+*/
+
+
+
+
+
+
+
+/* datos objetos */
