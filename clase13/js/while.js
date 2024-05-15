9dd552c8aa5e6b32af5f1785a5ae0f9701a0d4a2
Pablo Pereyra <143567596+Pablopereyra1976@users.noreply.github.com>
2024-05-14 21:26:16 -0300

    en while.js incorpore ejercicios js(matias)

--- clase13/js/while.js
+++ clase13/js/while.js
@@ -9,9 +9,69 @@ while(nombre === 'pepe'){
 }
 alert('ese nombre no lo conozco')*/
 
-let apellido = prompt('ingrese su apellido')
+/*  let apellido = prompt('ingrese su apellido')
 while (!apellido) {
     alert('error: DATO NO VALIDO')
 apellido = prompt('ingrese nuevamente su apellido')
 }
-alert('DATO CORRECTO')
+alert('DATO CORRECTO') */
+
+/* ********************************************* */
+
+/* TAREAS */
+
+/*  let tieneHijos = false
+if(tieneHijos){
+    console.log('tienes hijos')
+}
+else{
+    console.log('no tienes hijos')
+}
+
+
+
+let genero = prompt('ingrese su genero')
+if(genero === 'masculino'){
+    console.log('eres hombre')
+}
+else if(genero === 'femenino'){
+    console.log('eres mujer')
+}
+
+
+
+let lado = prompt('ingrese el valor de un lado del cuadrado')
+alert('el perimetro del cuadrado es: ' + lado * 4)
+
+
+
+let num1=2
+let num2=5
+let num3=7
+let num4=9
+alert('la suma es: '+(num1+num2))
+alert('y la multiplicacion es: ' +(num3*num4))
+
+
+
+let num = 100
+if(num > 0){
+    alert('el numero es positivo');
+}
+else if(num === 0){
+    alert('el numero es 0');     
+}
+else {
+    alert('el numero es negativo');
+}
+
+
+
+let tienePermiso = false;
+let edad = prompt("ingrese su edad");
+if (edad >= 18 && tienePermiso) {
+  console.log("puedes conducir");
+} else {
+  console.log("no puedes conducir");
+}
+*/
