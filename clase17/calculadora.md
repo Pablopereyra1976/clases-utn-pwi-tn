5404b19753cce241864335fa2e72bc5630dbf6a4
Pablo Pereyra <143567596+Pablopereyra1976@users.noreply.github.com>
2024-05-28 21:30:37 -0300

    Email/Password/Calculadora

--- clase17/calculadora.md
+++ clase17/calculadora.md
@@ -16,20 +16,73 @@ en caso de elegir una '+'
 aca sumamos
 mostrar por alerta 'el resultado de {numero(1)} {operacion} {numero2} es {resultado}
 
-en caso de elegir una '
--'
-aca sumamos
+en caso de elegir una '-'
+aca restamos
 mostrar por alerta 'el resultado de {numero(1)} {operacion} {numero2} es {resultado}
 
-
 HISTORIAL
-historial
+El historial debera tener el siguiente formato
+
+Dado el siguiente array
+historial = [
+    {
+        accion: 'CALCULAR',
+        operacion: '+',
+        a: 1,
+        b: 2,
+        resultado: 2 
+    },
+    {
+        accion: 'CALCULAR',
+        operacion: '+',
+         a: 1,
+        b: 2,
+        resultado: 2 
+    },
+]
+
+
+Crear una funcion llamada: renderizarHistorial(historial)
+
+La funcion debera crear un string a partir del array con el siguiente formato y retornarlo:
+
+'
+El historial es:
+
+Accion: CALCULAR
+operacion: {operacion}
+numeros: {a}, {b}
+resultado: {resultado}
+
+Accion: CALCULAR
+operacion: {operacion}
+numeros: {a}, {b}
+resultado: {resultado}
+'
+Luego mostar el string resultante por alerta
+
+Recuerda: puedes usar `` (template string para hacer saltos de linea) o '\n'
+Ejemplo:
+'El historial es:\n-hola' 
+se vera como:
+El historial es
+-hola
+
+o con template string:
+`
+El historial es
+-hola
+`
+se vera como:
+El historial es
+-hola
+
 
 LOGIN
 crear usuario (esta se ejecuta al entrar a la calculadora)
-  -usuario
-  -contraseña
-  -email => debe estar validado
+-usuario
+-contraseña
+-email => debe estar validado
 
 utilidades:
 validacion
@@ -42,7 +95,7 @@ egreso de datos:
 -alert
 
 LOGIN:
-prompt va a solicitar al ususario un email 
+prompt va a solicitar al ususario un email
 validamos que el email sea un email
 regEx / expresion regular: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(emailAVerificar) nos retorna un boolean
 en caso de que no volvera a solicitar
@@ -55,6 +108,6 @@ pueden usar todas las funciones o crear las funciones que creean necesarias
 
 reglas:
 todo el logeo debe estar dentro de una funcionalidad
-login() debe debe retornar un objeto usuario: { email:  , password:  }
+login() debe debe retornar un objeto usuario: { email: , password: }
 
-CALCULADORA
+
--- clase17/tareas.js
+++ clase17/tareas.js
@@ -1,72 +1,76 @@
-
-/*let emailUsuario = prompt('ingrese su email')
+let emailUsuario = prompt("ingrese su email");
 function loginEmail(emailUsuario) {
-    while (!(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(emailUsuario))) {
-        alert('error: DATO NO VALIDO')
-        emailUsuario = prompt('ingrese su email')
-    }
-    alert('DATO CORRECTO')
-
+  while (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g.test(emailUsuario)) {
+    alert("error: DATO NO VALIDO");
+    emailUsuario = prompt("ingrese su email");
+  }
+  alert("El Email " + emailUsuario + " se ha  registrado con exito");
 }
-loginEmail(emailUsuario)
 
+loginEmail(emailUsuario);
 
-let passwordUsuario = prompt('ingrese su password')
+let passwordUsuario = prompt("Ingrese su password");
+
 function loginPassword(passwordUsuario) {
-
-    while (passwordUsuario === null ||passwordUsuario.length > 6 && passwordUsuario === '') {
-        alert('error: DATO NO VALIDO')
-        passwordUsuario = prompt('ingrese su password')
-        alert('CONTRASEÑA CORRECTA')
-    }
-    
+  while (
+    passwordUsuario === null ||
+    passwordUsuario.length <= 6 ||
+    (passwordUsuario.trim() === "" ||
+     !/[A-Z]/.test(passwordUsuario) || 
+     !/[0-9]/.test(passwordUsuario))
+  ) {
+    alert('Error: La contraseña debe tener al menos 6 caracteres, incluir una letra mayúscula y un número.');
+    passwordUsuario = prompt("Ingrese su password");
+  }
+  alert("La contraseña " + passwordUsuario + " se ha registrado con exito");
 }
 
-loginPassword(passwordUsuario)
+loginPassword(passwordUsuario);
 
-
-
-function login(){
-    let email = emailUsuario()
-    let password = passwordUsuario()
-    return {email: email, password: password}
+function login() {
+  let email = emailUsuario();
+  let password = passwordUsuario();
+  return {
+    email: email,
+    password: password,
+  };
 }
-console.log(login)
+console.log(login);
 
-function loginPassword(passwordUsuario){    
-
-while(passwordUsuario === null || passwordUsuario.length > 6 && passwordUsuario === '')
-    alert('error: DATO NO VALIDO')
-    passwordUsuario = prompt('ingrese su password')
-    alert('CONTRASEÑA CORRECTA')}*/
-
-
-let operacion = prompt('ingrese la operacion + o -')
-while(operacion !== '+' && operacion !== '-'){
-    alert('error: DATO NO VALIDO')
-    operacion = prompt('ingrese la operacion + o -')
+let operacion = prompt("ingrese la operacion + o -");
+while (operacion !== "+" && operacion !== "-") {
+  alert("error: DATO NO VALIDO");
+  operacion = prompt("ingrese la operacion + o -");
 }
 
-let num1 = parseFloat(prompt('ingrese tu primer numero'))
-while(isNaN(num1) && num1 === ''){
-    alert('error: NO ES VALIDO')
-    num1 = parseFloat(prompt('ingrese tu primer numero'))
+let num1 = parseFloat(prompt("ingresa tu primer numero"));
+while (isNaN(num1) && num1 === "") {
+  alert("error: NO ES VALIDO");
+  num1 = parseFloat(prompt("ingresa tu primer numero"));
 }
-let num2 = parseFloat(prompt('ingrese tu segundo numero'))
-while(isNaN(num2) && num2 === ''){
-    alert('error: NO ES VALIDO')
-    num2 = parseFloat(prompt('ingrese tu segundo numero'))
-}   
-
-function calculadora() {         
-    if (operacion === '+') {
-    return num1 + num2
-}
-    if (operacion === '-') {
-    return num1 - num2
-}
+let num2 = parseFloat(prompt("ingresa tu segundo numero"));
+while (isNaN(num2) && num2 === "") {
+  alert("error: NO ES VALIDO");
+  num2 = parseFloat(prompt("ingresa tu segundo numero"));
 }
 
-alert(calculadora())
+function calculadora() {
+  if (operacion === "+") {
+    return "El resultado de esta operacion es: " + (num1 + num2);
+  }
+
+  if (operacion === "-") {
+    return "El resultado de esta operacion es: " + (num1 - num2);
+  }
+}
+
+alert(calculadora());
 
 
+
+function renderizarHistorial() {
+  let historial = document.getElementById("historial");
+  historial.innerHTML = historial.innerHTML + "<li>" + calculadora() + "</li>";
+}
+
+renderizarHistorial(historial);
